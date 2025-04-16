8338337a1afd2ef340e0a53d2e5149ad6de2ddd0
ChetanSawant <chetansawant.dev@gmail.com>
2025-04-16 07:37:18 +0530

    First Commit

---
+++ src/main/java/com/his/main/HisMainModuleApplication.java
@@ -0,0 +1,13 @@
+package com.his.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisMainModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisMainModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/his/main/HospitalInformationSystemMainModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.his.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalInformationSystemMainModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
